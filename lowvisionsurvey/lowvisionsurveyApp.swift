e4f2a2530018bd6cdc9f67e5081baab81196dd03
hedch <dongcheng.he@du.edu>
2024-02-20 13:39:35 -0800

    Initial Commit

---
+++ lowvisionsurvey/lowvisionsurveyApp.swift
@@ -0,0 +1,17 @@
+//
+//  lowvisionsurveyApp.swift
+//  lowvisionsurvey
+//
+//  Created by SELAB on 2/16/24.
+//
+
+import SwiftUI
+
+@main
+struct lowvisionsurveyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
